dc2e047c79dcecfaa077916e0d205c08949871f3
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 10:53:19 +0430

    l

--- src/div.jsx
+++ src/div.jsx
@@ -1,6 +1,6 @@
 function card() {
   return;
   <>
-    <div className="h-[300px] w-[250px] bg-blue-50"></div>
+    <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center"></div>
   </>;
 }
