42bde3569f1b81336a3f86dff46774980b82643a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 23:07:18 +0430

    l

--- src/div.jsx
+++ src/div.jsx
@@ -6,6 +6,11 @@ function div() {
         <div className="flex space-x-4">
           <h1 className="font-bold text-xl">Gmail</h1>
           <h1 className="font-bold text-xl">Images</h1>
+          <span className="font-bol text-xl">🚹</span>
+          <img
+            src="../img/WhatsApp Image 2025-04-29 at 08.24.50_65cf2dc8.jpg"
+            alt=""
+          />
         </div>
       </div>
     </>
