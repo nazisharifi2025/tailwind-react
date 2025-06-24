1038f0304f626196a92ec477b03336cea1ee0acb
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 11:00:04 +0430

    l

--- src/div.jsx
+++ src/div.jsx
@@ -3,7 +3,12 @@ function card() {
   <>
     <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center flex justify-center items-center shadow-md shadow-gray-300">
       <h1 className="text-center font-bold text-xl">Naz_Sharifi</h1>
-      <p className="text-center text-xs"></p>
+      <p className="text-center text-xs">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tempore
+        sequi optio itaque praesentium natus beatae dolore amet aspernatur
+        ullam, quia sunt pariatur cumque voluptatum nisi temporibus ea veniam
+        ducimus.
+      </p>
     </div>
   </>;
 }
