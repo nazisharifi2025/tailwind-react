663d969fca6c39704f0fb9aa72957869cb24c68e
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 23:15:58 +0430

    l

--- src/div.jsx
+++ src/div.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-function div() {
+function Card() {
   return (
     <>
-      <div className="h-20 w-full text-center p-4 flex  justify-end">
-        <div className="flex space-x-4">
-          <h1 className="font-bold text-xl">Gmail</h1>
-          <h1 className="font-bold text-xl">Images</h1>
-          <span className="font-bol text-xl">🚹</span>
+      <div className="h-20 w-full text-center p-4 flex  justify-right float-right items-end">
+        <div className="flex space-x-4 w-full justify-right float-right">
+          <h3 className=" text-xl font-bold">Gmail</h3>
+          <h2 className=" text-xl font-bold">Images</h2>
+          <span className="text-xl">🚹</span>
           <img
             src="../img/WhatsApp Image 2025-04-29 at 08.24.50_65cf2dc8.jpg"
             alt=""
@@ -17,4 +17,4 @@ function div() {
     </>
   );
 }
-export default div;
+export default Card;
