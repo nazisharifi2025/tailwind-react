b8b457fc582f0a0ee7bf8404ca5f36867e060db0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 22:57:22 +0430

    L

--- src/div.jsx
+++ src/div.jsx
@@ -1,14 +1,17 @@
-function card() {
-  return;
-  <>
-    <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center flex justify-center items-center shadow-md shadow-gray-300">
-      <h1 className="text-center font-bold text-xl">Naz_Sharifi</h1>
-      <p className="text-center text-xs">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tempore
-        sequi optio itaque praesentium natus beatae dolore amet aspernatur
-        ullam, quia sunt pariatur cumque voluptatum nisi temporibus ea veniam
-        ducimus.
-      </p>
-    </div>
-  </>;
+import "./App.css";
+function Card() {
+  return (
+    <>
+      <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center p-4 flex justify-center items-center shadow-md shadow-gray-300">
+        <h1 className="text-center font-bold text-xl">Naz_Sharifi</h1>
+        <p className="text-center text-xs">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+          tempore sequi optio itaque praesentium natus beatae dolore amet
+          aspernatur ullam, quia sunt pariatur cumque voluptatum nisi temporibus
+          ea veniam ducimus.
+        </p>
+      </div>
+    </>
+  );
 }
+export default Card;
