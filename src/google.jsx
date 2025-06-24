b9154e603d447a4da54dcd1d52b4b2c26677e27a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 23:24:11 +0430

    l

--- src/google.jsx
+++ src/google.jsx
@@ -3,6 +3,11 @@ function google() {
     <>
       <div className="flex justify-center items-center w-full h-16 font-bold text-2xl">
         <span className="text-blue-200">G</span>
+        <span className="text-red-300">o</span>
+        <span className="text-blue-200">o</span>
+        <span className="text-blue-200">g</span>
+        <span className="text-blue-200">l</span>
+        <span className="text-blue-200">e</span>
       </div>
     </>
   );
