2d22c250ed03bca33efd4e028d8b9673fbace3db
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 23:29:07 +0430

    l

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Card from "./div.jsx";
-
+import google from "./google.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Card />
+    <google />
   </StrictMode>
 );
