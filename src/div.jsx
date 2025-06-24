cc672c73b1729f5d9dd4ceb09c452c2fab55f0f5
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-24 10:57:06 +0430

    l

--- src/div.jsx
+++ src/div.jsx
@@ -1,6 +1,8 @@
 function card() {
   return;
   <>
-    <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center flex justify-center items-center shadow-md shadow-gray-300"></div>
+    <div className="h-[300px] w-[250px] bg-blue-50 rounded-md text-center flex justify-center items-center shadow-md shadow-gray-300">
+      <h1>Naz_Sharifi</h1>
+    </div>
   </>;
 }
