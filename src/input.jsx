a042353781f080fece3d0132db4c0865292eebe8
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-25 10:45:50 +0430

    l

--- src/input.jsx
+++ src/input.jsx
@@ -2,7 +2,10 @@ function Input() {
   return (
     <>
       <div className="w-full flex justify-center items-center">
-        <input type="text" className="h-12 w-[80%] rounded-full " />
+        <input
+          type="text"
+          className="h-12 w-[80%] rounded-full shadow-md shadow-gray-300"
+        />
       </div>
     </>
   );
