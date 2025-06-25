f8fe556693155b15d9e04edebdaffdcc23fad765
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-25 20:08:51 +0430

    l

--- src/Messeg.jsx
+++ src/Messeg.jsx
@@ -1,7 +1,7 @@
 export default function Messeg() {
   return (
     <>
-      <div className="h-16 w-full "></div>
+      <div className="h-16 w-full bg-blue-200 text-white"></div>
     </>
   );
 }
