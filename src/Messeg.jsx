bccef7ea9e357f1605f628784adbdb3cc3b53baa
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-25 20:12:27 +0430

    l

--- src/Messeg.jsx
+++ src/Messeg.jsx
@@ -1,7 +1,9 @@
 export default function Messeg() {
   return (
     <>
-      <div className="h-16 w-full bg-blue-200 text-white rounded-md shadow-md shadow-gray-300 flex justify-center items-center gap-3"></div>
+      <div className="h-fit p-6 w-full bg-blue-200 text-white rounded-md shadow-md shadow-gray-300 flex justify-center items-center gap-3">
+        <h1>In the name Of allah</h1>
+      </div>
     </>
   );
 }
