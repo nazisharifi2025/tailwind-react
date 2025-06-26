adce448a9fca2caa8019c264272538b7810381bf
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-26 10:25:30 +0430

    l

--- src/js.jsx
+++ src/js.jsx
@@ -1,8 +1,10 @@
-export default function Js() {
+export default function Js(propes) {
   return (
     <>
       <div className="h-12 w-12 bg-gray-400 text-white flex justify-center items-center text-xl font-bold">
-        <h1>Text</h1>
+        <h1>
+          Hi my name is {propes.name} my LastName is {propes.lastName}
+        </h1>
       </div>
     </>
   );
