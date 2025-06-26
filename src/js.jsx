25f4851a60cdbe95fcee583596694e0e9809c95f
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-26 10:05:12 +0430

    lL

--- src/js.jsx
+++ src/js.jsx
@@ -1,7 +1,9 @@
 export default function Js() {
   return (
     <>
-      <div className="h-12 w-12 bg-gray-400 text-white flex justify-center items-center text-xl font-bold"></div>
+      <div className="h-12 w-12 bg-gray-400 text-white flex justify-center items-center text-xl font-bold">
+        <h1>Text</h1>
+      </div>
     </>
   );
 }
