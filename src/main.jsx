73bb3ff814be425e1e835a6d42184104e86688e3
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-26 10:38:23 +0430

    l

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Card from "./div.jsx";
 import Google from "./google.jsx";
 import Input from "./input.jsx";
 import Icone from "../Icons.jsx";
-import Messeg from "./Messeg.jsx";
+import Js from "./js.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
@@ -14,6 +14,6 @@ createRoot(document.getElementById("root")).render(
     <Google />
     <Input />
     <Icone />
-    <Messeg />
+    <Js />
   </StrictMode>
 );
