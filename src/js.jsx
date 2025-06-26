fcfb2f609862684512981ea2d60c6e4155334e81
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-26 11:01:34 +0430

    l

--- src/js.jsx
+++ src/js.jsx
@@ -1,9 +1,10 @@
 export default function Js(propes) {
+  const { Mess } = propes;
   return (
     <>
       <div className="h-12 w-12 bg-gray-400 text-white flex justify-center items-center text-xl font-bold">
         <h1>
-          Hi my name is {propes.name} my LastName is {propes.LastName} and Iam{" "}
+          Hi my name is {Mess} my LastName is {propes.LastName} and Iam{" "}
           {propes.age} years old.
         </h1>
       </div>
