2845f670d32c8782cc5ee7638f4b5ade0ce57170
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-26 10:42:18 +0430

    l

--- src/js.jsx
+++ src/js.jsx
@@ -3,7 +3,7 @@ export default function Js(propes) {
     <>
       <div className="h-12 w-12 bg-gray-400 text-white flex justify-center items-center text-xl font-bold">
         <h1>
-          Hi my name is {propes.name} my LastName is {propes.lastName} and Iam{" "}
+          Hi my name is {propes.name} my LastName is {propes.LastName} and Iam{" "}
           {propes.age} years old.
         </h1>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Card from "./div.jsx";
 import Google from "./google.jsx";
 import Input from "./input.jsx";
 import Icone from "../Icons.jsx";
-import Js from "./js.jsx";
+import Mess from "./mess.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
@@ -14,6 +14,6 @@ createRoot(document.getElementById("root")).render(
     <Google />
     <Input />
     <Icone />
-    <Js />
+    <Mess />
   </StrictMode>
 );
