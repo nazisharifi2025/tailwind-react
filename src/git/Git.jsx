aea891c4cc7545c823e956c3d89f0b764130b352
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-28 21:39:42 +0430

    l

--- src/git/Git.jsx
+++ src/git/Git.jsx
@@ -1,7 +1,12 @@
 function Git() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-200 text-white flex justify-center items-center gap-7"></div>
+      <div className="h-screen w-full bg-gray-200 text-white flex justify-center items-center gap-7 shadow-md shadow-gray-400">
+        <img
+          src="../img/WhatsApp Image 2025-04-29 at 08.24.50_65cf2dc8.jpg"
+          alt=""
+        />
+      </div>
     </>
   );
 }
