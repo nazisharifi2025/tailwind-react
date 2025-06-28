4fbc0bd868c6e747eba2e47862f3607934d4ef50
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-28 21:41:39 +0430

    l

--- src/git/Git.jsx
+++ src/git/Git.jsx
@@ -1,12 +1,13 @@
 function Git() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-200 text-white flex justify-center items-center gap-7 shadow-md shadow-gray-400">
+      <div className="h-screen flex-col w-full bg-gray-200 text-white flex justify-center items-center gap-7 shadow-md shadow-gray-400">
         <img
           src="../img/WhatsApp Image 2025-04-29 at 08.24.50_65cf2dc8.jpg"
           className="h-[300px] w-[250px] rounded-2xl"
           alt=""
         />
+        <h1>Devloper</h1>
       </div>
     </>
   );
