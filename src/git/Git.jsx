b93e53a95c8f75f6bcdf3b464360defac6a9455a
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-28 21:40:19 +0430

    l

--- src/git/Git.jsx
+++ src/git/Git.jsx
@@ -4,6 +4,7 @@ function Git() {
       <div className="h-screen w-full bg-gray-200 text-white flex justify-center items-center gap-7 shadow-md shadow-gray-400">
         <img
           src="../img/WhatsApp Image 2025-04-29 at 08.24.50_65cf2dc8.jpg"
+          className="h-[300px] w-[250px]"
           alt=""
         />
       </div>
