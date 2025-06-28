92687ff5d2d9512f64a7f34cd14a1db2f1d19153
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-28 21:37:37 +0430

    l

--- src/git/Git.jsx
+++ src/git/Git.jsx
@@ -1,7 +1,7 @@
 function Git() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-200 text-white flex justify-center"></div>
+      <div className="h-screen w-full bg-gray-200 text-white flex justify-center items-center gap-7"></div>
     </>
   );
 }
